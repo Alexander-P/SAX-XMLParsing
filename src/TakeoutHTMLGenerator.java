8184678a0ca5d014e7c94c8892ab82b55f846fc8
Alexander-P <pollek@gmx.at>
2024-12-18 09:41:20 +0100

    Restructured code and added personnel details

--- src/TakeoutHTMLGenerator.java
+++ src/TakeoutHTMLGenerator.java
@@ -1,3 +1,4 @@
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -21,73 +22,23 @@ public class TakeoutHTMLGenerator {
       File inputFile = new File(TAKEOUT_XML);
       FileWriter htmlWriter = new FileWriter("takeout.html");
 
-      htmlWriter.write("<html><head><title>Takeout Menu and Orders</title></head><body>");
-
-      Map<String, String> dishMap = new HashMap<>(); // Map to store dish ID and names
-      Map<String, String> delivererMap = new HashMap<>(); // Map to store deliverer ID and names
+      htmlWriter.write("<html><head><title>Takeout Document</title></head><body>");
 
       DefaultHandler handler = new DefaultHandler() {
         private StringBuilder currentValue = new StringBuilder();
-        private boolean inMenu = false;
-        private boolean inOrders = false;
+        private Map<String, String> dishMap = new HashMap<>();
+        private Map<String, String> personnelMap = new HashMap<>();
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
           currentValue.setLength(0);
-          if (qName.equals("offered-dishes")) {
-            inMenu = true;
-            try {
-              htmlWriter.write("<h2>Menu</h2><ul>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("dish")) {
-            String dishId = attributes.getValue("id");
-            String dishName = attributes.getValue("name");
-            dishMap.put(dishId, dishName);
-            try {
-              htmlWriter.write("<li>" + dishName + " (Vegetarian: " + attributes.getValue("vegetarian") + ")</li>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("person")) {
-            String personId = attributes.getValue("id");
-            String personName = null;
-          } else if (qName.equals("current-orders")) {
-            inOrders = true;
-            try {
-              htmlWriter.write("<h2>Orders</h2>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("order")) {
-            try {
-              htmlWriter.write("<h3>Order ID: " + attributes.getValue("order-id") + "</h3><ul>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("item")) {
-            String dishId = attributes.getValue("dish");
-            String dishName = dishMap.getOrDefault(dishId, "Unknown Dish");
-            try {
-              htmlWriter.write("<li>Dish: " + dishName + ", Price: " + attributes.getValue("price") + "</li>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("address")) {
-            String deliveredBy = attributes.getValue("deliveredBy");
-            String delivererName = delivererMap.getOrDefault(deliveredBy, "Unknown Deliverer");
-            try {
-              htmlWriter.write("<li>Delivery Address: " + currentValue.toString() + " (Delivered By: " + delivererName + ")</li>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("self-pickup")) {
-            try {
-              htmlWriter.write("<li>Self-Pickup by: " + attributes.getValue("client-name") + "</li>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
+          try {
+            handleTakeoutStart(qName, attributes);
+            handleOfferedDishesStart(qName, attributes);
+            handlePersonnelStart(qName, attributes);
+            handleOrdersStart(qName, attributes);
+          } catch (IOException e) {
+            throw new SAXException(e);
           }
         }
 
@@ -98,19 +49,102 @@ public class TakeoutHTMLGenerator {
 
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
-          if (qName.equals("offered-dishes")) {
-            inMenu = false;
-            try {
+          try {
+            handleNameEnd(qName);
+            handleTransportEnd(qName);
+            handleAddressEnd(qName);
+            handleGenericEnd(qName);
+          } catch (IOException e) {
+            throw new SAXException(e);
+          }
+        }
+
+        private void handleTakeoutStart(String qName, Attributes attributes) throws IOException {
+          if ("takeout".equals(qName)) {
+            htmlWriter.write("<h1>Takeout Document</h1>");
+          }
+        }
+
+        private void handleOfferedDishesStart(String qName, Attributes attributes) throws IOException {
+          switch (qName) {
+            case "offered-dishes":
+              htmlWriter.write("<h2>Menu</h2><ul>");
+              break;
+            case "dish":
+              String dishId = attributes.getValue("id");
+              String dishName = attributes.getValue("name");
+              dishMap.put(dishId, dishName);
+              htmlWriter.write("<li>" + dishName + " (Vegetarian: " + attributes.getValue("vegetarian") + ")</li>");
+              break;
+          }
+        }
+
+        private void handlePersonnelStart(String qName, Attributes attributes) throws IOException {
+          switch (qName) {
+            case "personnel":
+              htmlWriter.write("<h2>Personnel</h2><ul>");
+              break;
+            case "person":
+              String personId = attributes.getValue("id");
+              personnelMap.put(personId, null);
+              htmlWriter.write("<li>ID: " + personId + "<br>");
+              break;
+          }
+        }
+
+        private void handleOrdersStart(String qName, Attributes attributes) throws IOException {
+          switch (qName) {
+            case "current-orders":
+              htmlWriter.write("</ul><h2>Orders</h2>");
+              break;
+            case "order":
+              htmlWriter.write("<h3>Order ID: " + attributes.getValue("order-id") + "</h3><ul>");
+              break;
+            case "address":
+              String deliveredBy = attributes.getValue("deliveredBy");
+              String driverName = personnelMap.getOrDefault(deliveredBy, "Unknown Driver");
+              htmlWriter.write("<li>Delivered By: " + driverName + " (ID: " + deliveredBy + ")</li>");
+              break;
+            case "self-pickup":
+              htmlWriter.write("<li>Self-Pickup (Client Name: " + attributes.getValue("client-name") + ")</li>");
+              break;
+            case "item":
+              String itemDishId = attributes.getValue("dish");
+              String itemDishName = dishMap.getOrDefault(itemDishId, "Unknown Dish");
+              htmlWriter.write("<li>Dish: " + itemDishName + ", Price: " + attributes.getValue("price") + "</li>");
+              break;
+          }
+        }
+
+        private void handleNameEnd(String qName) throws IOException {
+          if ("name".equals(qName)) {
+            String name = currentValue.toString().trim();
+            personnelMap.entrySet().stream()
+                .filter(entry -> entry.getValue() == null)
+                .findFirst()
+                .ifPresent(entry -> personnelMap.put(entry.getKey(), name));
+            htmlWriter.write(name + "<br>");
+          }
+        }
+
+        private void handleTransportEnd(String qName) throws IOException {
+          if ("transport".equals(qName)) {
+            htmlWriter.write("Transport: " + currentValue.toString().trim() + "</li>");
+          }
+        }
+
+        private void handleAddressEnd(String qName) throws IOException {
+          if ("address".equals(qName)) {
+            htmlWriter.write("<li>Address: " + currentValue.toString().trim() + "</li>");
+          }
+        }
+
+        private void handleGenericEnd(String qName) throws IOException {
+          switch (qName) {
+            case "order":
+            case "personnel":
               htmlWriter.write("</ul>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
-          } else if (qName.equals("order")) {
-            try {
-              htmlWriter.write("</ul>");
-            } catch (IOException e) {
-              throw new SAXException(e);
-            }
+              break;
           }
         }
       };
