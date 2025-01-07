1b2978a2ea6423f46a51c0e3ddb1b594b554045c
Alexander-P <pollek@gmx.at>
2025-01-07 10:45:28 +0100

    Moved Handler extension to private class

--- src/RestaurantXMLProcessor.java
+++ src/RestaurantXMLProcessor.java
@@ -25,110 +25,114 @@ public class RestaurantXMLProcessor {
       SAXParser saxParser = factory.newSAXParser();
       File inputFile = new File(EX6_XML);
 
-      DefaultHandler handler = new DefaultHandler() {
-        private int menuElementCount = 0;
-        private String restaurantName = "";
-        private String city = "";
-        private String country = "";
-        private int openDaysCount = 0;
-        private String longestDay = "";
-        private long longestDuration = 0;
-        private double totalDishPrice = 0.0;
-        private int dishCount = 0;
-        private double mostExpensiveDish = Double.MIN_VALUE;
-        private double cheapestDish = Double.MAX_VALUE;
-        private final Map<String, String> dayOpeningHours = new HashMap<>();
+      DefaultHandler handler = new RestaurantHandler();
+      saxParser.parse(inputFile, handler);
 
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-          if (qName.equals("restaurant")) {
-            restaurantName = attributes.getValue("name");
-          } else if (qName.equals("dish")) {
-            menuElementCount++;
-            double price = Double.parseDouble(attributes.getValue("price"));
-            totalDishPrice += price;
-            dishCount++;
-            mostExpensiveDish = Math.max(mostExpensiveDish, price);
-            cheapestDish = Math.min(cheapestDish, price);
-          } else if (qName.equals("drink")) {
-            menuElementCount++;
-          } else if (qName.equals("city")) {
-            city = attributes.getValue("value");
-          } else if (qName.equals("country")) {
-            country = attributes.getValue("value");
-          } else if (qName.equals("day")) {
-            String dayName = attributes.getValue("name");
-            String closed = attributes.getValue("closed");
-            if (!"true".equals(closed)) {
-              openDaysCount++;
-              currentDay = dayName;
+    } catch (
+        Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  private static class RestaurantHandler extends DefaultHandler {
+    private int menuElementCount = 0;
+    private String restaurantName = "";
+    private String city = "";
+    private String country = "";
+    private int openDaysCount = 0;
+    private String longestDay = "";
+    private long longestDuration = 0;
+    private double totalDishPrice = 0.0;
+    private int dishCount = 0;
+    private double mostExpensiveDish = Double.MIN_VALUE;
+    private double cheapestDish = Double.MAX_VALUE;
+    private final Map<String, String> dayOpeningHours = new HashMap<>();
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+      if (qName.equals("restaurant")) {
+        restaurantName = attributes.getValue("name");
+      } else if (qName.equals("dish")) {
+        menuElementCount++;
+        double price = Double.parseDouble(attributes.getValue("price"));
+        totalDishPrice += price;
+        dishCount++;
+        mostExpensiveDish = Math.max(mostExpensiveDish, price);
+        cheapestDish = Math.min(cheapestDish, price);
+      } else if (qName.equals("drink")) {
+        menuElementCount++;
+      } else if (qName.equals("city")) {
+        city = attributes.getValue("value");
+      } else if (qName.equals("country")) {
+        country = attributes.getValue("value");
+      } else if (qName.equals("day")) {
+        String dayName = attributes.getValue("name");
+        String closed = attributes.getValue("closed");
+        if (!"true".equals(closed)) {
+          openDaysCount++;
+          currentDay = dayName;
+        }
+      }
+    }
+
+    private String currentDay = null;
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+      String content = new String(ch, start, length).trim();
+      if (!content.isEmpty() && currentDay != null) {
+        dayOpeningHours.put(currentDay, content);
+      }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+      if (qName.equals("day")) {
+        currentDay = null;
+      }
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+      try (FileWriter file = new FileWriter(EX6_OUT_JSON)) {
+        calculateLongestDay();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("restaurantName", restaurantName);
+        jsonObject.put("menuElementCount", menuElementCount);
+        jsonObject.put("city", city);
+        jsonObject.put("country", country);
+        jsonObject.put("openDaysCount", openDaysCount);
+        jsonObject.put("longestDay", longestDay);
+        jsonObject.put("averageDishPrice", dishCount > 0 ? totalDishPrice / dishCount : 0.0);
+        jsonObject.put("mostExpensiveDish", mostExpensiveDish);
+        jsonObject.put("cheapestDish", cheapestDish);
+        file.write(jsonObject.toString(4)); // Pretty print with an indent of 4
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    private void calculateLongestDay() {
+      SimpleDateFormat format = new SimpleDateFormat("hh:mm a");
+      for (Map.Entry<String, String> entry : dayOpeningHours.entrySet()) {
+        String day = entry.getKey();
+        String hours = entry.getValue();
+        if (hours != null) {
+          String[] splitHours = hours.split(" - ");
+          try {
+            Date start = format.parse(splitHours[0]);
+            Date end = format.parse(splitHours[1]);
+            long duration = end.getTime() - start.getTime();
+            if (duration > longestDuration) {
+              longestDuration = duration;
+              longestDay = day;
             }
-          }
-        }
-        private String currentDay = null;
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-          String content = new String(ch, start, length).trim();
-          if (!content.isEmpty() && currentDay != null) {
-            dayOpeningHours.put(currentDay, content);
-          }
-        }
-
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-          if (qName.equals("day")) {
-            currentDay = null;
-          }
-        }
-
-        @Override
-        public void endDocument() throws SAXException {
-          try (FileWriter file = new FileWriter(EX6_OUT_JSON)) {
-            calculateLongestDay();
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("restaurantName", restaurantName);
-            jsonObject.put("menuElementCount", menuElementCount);
-            jsonObject.put("city", city);
-            jsonObject.put("country", country);
-            jsonObject.put("openDaysCount", openDaysCount);
-            jsonObject.put("longestDay", longestDay);
-            jsonObject.put("averageDishPrice", dishCount > 0 ? totalDishPrice / dishCount : 0.0);
-            jsonObject.put("mostExpensiveDish", mostExpensiveDish);
-            jsonObject.put("cheapestDish", cheapestDish);
-            file.write(jsonObject.toString(4)); // Pretty print with an indent of 4
-          } catch (IOException e) {
+          } catch (ParseException e) {
             e.printStackTrace();
           }
         }
-
-        private void calculateLongestDay() {
-          SimpleDateFormat format = new SimpleDateFormat("hh:mm a");
-          for (Map.Entry<String, String> entry : dayOpeningHours.entrySet()) {
-            String day = entry.getKey();
-            String hours = entry.getValue();
-            if (hours != null) {
-              String[] splitHours = hours.split(" - ");
-              try {
-                Date start = format.parse(splitHours[0]);
-                Date end = format.parse(splitHours[1]);
-                long duration = end.getTime() - start.getTime();
-                if (duration > longestDuration) {
-                  longestDuration = duration;
-                  longestDay = day;
-                }
-              } catch (ParseException e) {
-                e.printStackTrace();
-              }
-            }
-          }
-        }
-      };
-
-      saxParser.parse(inputFile, handler);
-
-    } catch (Exception e) {
-      e.printStackTrace();
+      }
     }
   }
 }
